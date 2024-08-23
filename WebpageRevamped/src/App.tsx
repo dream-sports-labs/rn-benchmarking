381d4ecbd6237196dfcf8152e27dc5c9550d5a93
poonamjain96 <poonam.jain@dream11.com>
2024-08-23 17:35:37 +0530

    fix: added ga

--- WebpageRevamped/src/App.tsx
+++ WebpageRevamped/src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
 import Home from "./components/home/Home";
+import {useEffect} from "react";
+import ReactGA from 'react-ga4';
 
 function App() {
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-JCMS7CG87C'); // Replace 'G-XXXXXXXXXX' with your actual Measurement ID
+
+    // Track the initial page load
+    ReactGA.send('pageview');
+  }, []);  // The empty dependency array ensures this runs once on mount
   return (
     <Home/>
   );
