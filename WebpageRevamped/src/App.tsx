9b4383e6a7a816f0ef9678e3b9eb638f471a5f57
poonamjain96 <poonam.jain@dream11.com>
2024-08-30 12:56:54 +0530

    fix: added app tsx file

--- WebpageRevamped/src/App.tsx
+++ WebpageRevamped/src/App.tsx
@@ -1,26 +1,13 @@
 import './App.css';
-import Home from "./components/home/Home";
-import {useEffect} from "react";
-import ReactGA from 'react-ga4';
+import Home from './components/home/Home';
+import useGoogleAnalytics from './hooks/useGoogleAnalytics';
 
 function App() {
-  const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TOKEN;
-
-  useEffect(() => {
-    if (trackingId) {
-      // Initialize Google Analytics using the tracking ID from environment variables
-      ReactGA.initialize(trackingId);
-
-      // Track the initial page load
-      ReactGA.send('pageview');
-    } else {
-      console.warn('Google Analytics tracking ID is not set.');
-    }
-  }, []);// The empty dependency array ensures this runs once on mount
-
-  return (
-    <Home/>
-  );
+    const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TOKEN;
+    useGoogleAnalytics(trackingId);
+    return (
+        <Home/>
+    );
 }
 
 export default App;
